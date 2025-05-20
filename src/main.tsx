f5021ff850f1584f2aba1e523d4e6b900203af83
NicksonJacksonWilfest <devxjr.pro@gmail.com>
2025-05-20 17:24:33 +0300

    🐞🐞 Added some components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contacts",
+    element: <Contact />,
+  },
+])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
+    <Footer />
   </StrictMode>,
 );
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+  return (
+    <>
+      This is Home Page
+    </>
+  )
+}
+export default HomePage
